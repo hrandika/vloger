c0dde9bdd4db7a9640da3469a59ed4714adc2167
randika <hrandika@hotmail.com>
2020-07-23 01:49:35 +0530

    ui changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2 } from '@angular/core';
 import { MatIconRegistry } from '@angular/material/icon';
 import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Select, Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Store } from '@ngxs/store';
 import { DarkMode } from 'src/shared/state/app/app.action';
 import { AppState } from 'src/shared/state/app/app.state';
 @Component({
@@ -12,19 +11,41 @@ import { AppState } from 'src/shared/state/app/app.state';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  @Select(AppState.darkMode)
-  public darkMode$: Observable<boolean>;
+  public white: boolean = false;
+  public dark: boolean = false;
+  public isDark: boolean = false;
+  private scroll: number;
 
   constructor(
     private store: Store,
     private iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer
+    private sanitizer: DomSanitizer,
+    private renderer: Renderer2
   ) {
     this.iconRegistry.addSvgIconSetInNamespace(
       'app',
       this.sanitizer.bypassSecurityTrustResourceUrl('assets/svg/app.svg')
     );
+    this.store.select(AppState.darkMode).subscribe((d) => {
+      this.isDark = d;
+      this.updateTheme();
+    });
+
+    this.renderer.listen('window', 'scroll', (event) => {
+      this.scroll = window.scrollY;
+      this.updateTheme();
+    });
   } //constructoru
+
+  private updateTheme() {
+    if (this.scroll > 0 || window.pageYOffset > 0) {
+      if (this.isDark) this.dark = true;
+      else this.white = true;
+    } else {
+      this.white = false;
+      this.dark = false;
+    }
+  }
 
   public onDarkMode(event: MatSlideToggleChange) {
     this.store.dispatch(new DarkMode(event.checked));
